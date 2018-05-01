4fb5816fec13bf17fa3532899449c35ef4498774
EucalyptusLeaves <leavessoft09@gmail.com>
2018-05-01 08:45:04 +0800

    new file:   API/droptoken.php

---
+++ API/droptoken.php
@@ -0,0 +1,49 @@
+<?php
+// UniBan AccessToken API
+// ✝️ God bless us.
+// Copyright (C) EucalyptusLeaves 2018
+if(!defined("UBSecurity")) exit("Access denied.");
+
+$retjson = 
+[	"result"	=>	"failed",
+	"reason"	=>	""
+];
+$isFailed = false;
+
+if ($_POST["token"]=="") {
+	$retjson["result"] = "failed";
+	$retjson["reason"] = "Incompleted argument:token";
+	$isFailed=true;
+}
+
+checkInput(); //注入检测
+
+if($isFailed) exit(json_encode($retjson));
+
+$userIP = $_SERVER['REMOTE_ADDR'];
+$server=$Mysql->get_row("SELECT * FROM servers WHERE token='".$_POST["token"]."'");
+if($server!=false) {
+    if($server['ip']!=$userIP) {
+        $retjson["result"] = "failed";
+        $retjson["reason"] = "Server IP not matched";
+        exit(json_encode($retjson));
+    }
+    else {
+        $token=getToken($server['serverkey']);
+        if ($token==false) {
+            $retjson["result"] = "failed";
+            $retjson["reason"] = "Failed droping token";
+        }
+        else {
+            $retjson['result'] = "OK";
+        }
+        exit(json_encode($retjson));
+    }
+    
+}
+else {
+	$retjson["result"] = "failed";
+	$retjson["reason"] = "Server not registered or token not correct";
+	exit(json_encode($retjson));
+}
+?>
